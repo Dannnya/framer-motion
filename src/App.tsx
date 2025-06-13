20796ba3cd5586a455c6f00b95723d607684fce3
Dannnya <komarovskiy.daniil23@gmail.com>
2025-06-13 23:40:06 +0300

    feat: Bouncing Loader

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { motion } from 'framer-motion';
+import { BouncingLoader } from './components/BouncingLoader';
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <h3 className="text-3xl font-bold underline ">Hello World!</h3>
+      {/* <motion.div className="box" animate={{ scale: [1, 2, 3, 2, 1] }} transition={{ duration: 5 }} /> */}
+
+      <BouncingLoader />
+    </div>
+  )
+}
+
+export default App
