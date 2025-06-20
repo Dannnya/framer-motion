25da575cc69c8f5eb3b0dfa8c9476c4bfd12395b
Dannnya <komarovskiy.daniil23@gmail.com>
2025-06-20 18:12:54 +0300

    update: Toast Notification Component

--- src/components/Exercises/ToastNotification.tsx
+++ src/components/Exercises/ToastNotification.tsx
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import { useState } from 'react';
 
 export const ToastNotification = () => {
@@ -11,7 +11,9 @@ export const ToastNotification = () => {
         setTimeout(() => {
             setVisible(false)
         }, 3000)
+
     }
+    
 
     return (
         <div className='flex flex-col items-center mt-10'>
@@ -21,12 +23,19 @@ export const ToastNotification = () => {
             >
                 Show notification
             </button>
-
+            <AnimatePresence>
             {visible && (
-                <motion.div className='fixed top-4 right-4 p-4 bg-green-500 text-white rounded'>
+                <motion.div
+                    className='fixed top-4 right-4 p-4 bg-green-500 text-white rounded'
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: -20 }}
+                    transition={{ duration: 0.5 }}
+                >
                     Notification: Action Successfull !
                 </motion.div>
-            )}
+                )}
+            </AnimatePresence>
         </div>
     )
 };
